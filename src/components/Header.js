7ff939324b7d31bd0552b37c6c1b55aa4160ffc3
Shahidh K Muhammed <shahidh@hasura.io>
2020-03-24 12:36:13 +0530

    update images

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,20 @@ import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import { useAuth0 } from "../react-auth0-spa";
 import { LinkContainer } from "react-router-bootstrap";
+import logo from '../images/logo.png';
 
 const Header = () => {
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-      <Navbar.Brand href="#home">LilBerry</Navbar.Brand>
+      <Navbar.Brand href="#">
+        <img
+          src={logo}
+          height="50"
+          className="d-inline-block align-top"
+          alt="LilBerry logo"
+        />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
