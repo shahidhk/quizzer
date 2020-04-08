8d8a9de2def03d210e5bd45ba89f1f584f859849
Shahidh K Muhammed <shahidh@hasura.io>
2020-04-08 13:30:33 +0530

    change no exam text

--- src/components/Home.js
+++ src/components/Home.js
@@ -96,7 +96,7 @@ const LiveQuiz = () => {
 
   if (data && data.quiz && data.quiz.length === 0) {
     // we don't have a quiz
-    return <div>Sorry, there are no quizzes live right now. Check back later!</div>
+    return <div>Exam will start at 2pm on April 8th 2020. Check back later!</div>
   }
 }
 
