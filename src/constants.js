b07859f57ad3014f5754e505eb54379393d290f9
Shahidh K Muhammed <shahidh@hasura.io>
2020-07-22 08:00:04 +0530

    update description

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,13 @@
 export const brand = {
-  title: 'Teenspace Contest',
-  description: `Teenspace contest by Wisdom Students`,
+  title: 'Teen Contest',
+  description: `Teen Contest is a monthly quiz conducted under the banner of Teenspace by Wisdom Students. Students from 7th to 12th Standard are encouraged to participate. Topic will be given beforehand`,
   image_url: '/logo.png',
   navbar_logo_url: '/navbar_logo.png',
   rules: `<p>Teenspace എന്ന കൗമാരപ്രായത്തിലുള്ള വിദ്യാർത്ഥികൾക്കായി സംഘടിപ്പിക്കപ്പെടുന്ന പദ്ധതിയുടെ ഭാഗമായി  നടത്തുന്ന TEENCONTEST ആണ് നിങ്ങൾ ഇപ്പോൾ അറ്റൻഡ് ചെയ്യാൻ പോകുന്നത്. ഇത് ഇന്ത്യയിലും വിദേശത്തും സാധുതയുള്ളതാണ്.</p>
 
   <p>TEEN SPACE, an exclusive programme organized for teenagers,  is conducting a quiz competition “TEEN CONTEST” which you are about to attend. Participants can attend this contest from INDIA AND ABROAD.</p>
   
-  <h4>യോഗ്യത/Eligibility</h4>
+  <h4>യോഗ്യത (Eligibility)</h4>
   
   <p>താഴെ പറയുന്ന യോഗ്യതാ മാനദണ്ഡങ്ങൾ ഉള്ളവർക്ക് മാത്രമേ മത്സരത്തി പങ്കെടുക്കാവൂ :
   (To be eligible for contest, participants should follow the below criteria:)</p>
@@ -22,25 +22,20 @@ export const brand = {
   <ul>
   <li> സമയപരിധിക്കുള്ളിൽ ( ഉച്ചകഴിഞ്ഞ് 2 മുതൽ രാത്രി 10 വരെ [IST] ) നേരത്തെ പറഞ്ഞ, യോഗ്യതയുള്ള ആർക്കും, കൃത്യം രണ്ട് മണിക്ക് പുറത്ത് വരുന്ന ലിങ്കിലൂടെ മത്സരത്തിൽ പങ്കെടുക്കാം. (Eligible participants can join the contest through a link which will be shared between  afternoon 2 pm and 10pm.)</li>
   
-  <li>നേരത്തെ അറിയിക്കുന്ന വിഷയവുമായി ബന്ധപ്പെട്ട 20 ചോദ്യങ്ങളായിരിക്കും ഉണ്ടാവുക. ഓരോ ചോദ്യത്തിനും 4 ഓപ്ഷൻസ് വീതം ഉണ്ടായിരിക്കും.</li>
-  <li>20 Multiple choice question regarding the topic will be given. Each question  have 4 multiple choice to choose from. </li>
+  <li>നേരത്തെ അറിയിക്കുന്ന വിഷയവുമായി ബന്ധപ്പെട്ട 20 ചോദ്യങ്ങളായിരിക്കും ഉണ്ടാവുക. ഓരോ ചോദ്യത്തിനും 4 ഓപ്ഷൻസ് വീതം ഉണ്ടായിരിക്കും. (20 Multiple choice question regarding the topic will be given. Each question  have 4 multiple choice to choose from.)</li>
   
-  <li>പരീക്ഷ തുടങ്ങി 1/2 മണിക്കൂറിനുള്ളിൽ മുഴുവൻ ചോദ്യങ്ങൾക്കും ഉത്തരം നൽകേണ്ടതാണ്. </li>
-  <li>Allotted time to answer all the questions is 30 MINUTES FROM START TO END. </li>
+  <li>പരീക്ഷ തുടങ്ങി 1/2 മണിക്കൂറിനുള്ളിൽ മുഴുവൻ ചോദ്യങ്ങൾക്കും ഉത്തരം നൽകേണ്ടതാണ് (Allotted time to answer all the questions is 30 MINUTES FROM START TO END)</li>
   
-  <li>അതാത് ക്ലാസ്സുകളിൽ ഏറ്റവും കൂടുതൽ സ്കോർ നേടുന്നവരിൽ നിന്ന് തെരഞ്ഞെടുക്കുന്ന വിജയികൾക്ക് ആകർഷകമായ സമ്മാനങ്ങൾ ഉണ്ടായിരിക്കും.</li>
-  <li>Participants with higher score will be selected from the appointed classes and will be “AWARDED WITH EXCITING PRIZES”</li>
+  <li>അതാത് ക്ലാസ്സുകളിൽ ഏറ്റവും കൂടുതൽ സ്കോർ നേടുന്നവരിൽ നിന്ന് തെരഞ്ഞെടുക്കുന്ന വിജയികൾക്ക് ആകർഷകമായ സമ്മാനങ്ങൾ ഉണ്ടായിരിക്കും. (Participants with higher score will be selected from the appointed classes and will be “AWARDED WITH EXCITING PRIZES”)</li>
   
-  <li>ഏറ്റവും കൂടുതൽ ചോദ്യങ്ങൾ‌ക്ക് ശരിയായ ഉത്തരം നൽ‌കുന്ന ഒന്നിലധികം പേർ ഉണ്ടെങ്കിൽ, random draw - ലൂടെ വിജയികളെ തിരഞ്ഞെടുക്കും.</li>
-  <li>The winner will be chosen by “RANDOM DRAW” if scores achieved by more than 1 participants is equal. </li>
+  <li>ഏറ്റവും കൂടുതൽ ചോദ്യങ്ങൾ‌ക്ക് ശരിയായ ഉത്തരം നൽ‌കുന്ന ഒന്നിലധികം പേർ ഉണ്ടെങ്കിൽ, random draw - ലൂടെ വിജയികളെ തിരഞ്ഞെടുക്കും (The winner will be chosen by “RANDOM DRAW” if scores achieved by more than 1 participants is equal)</li>
   
-  <li>വിജയികളാവുന്നവർ 'വിദ്യാർത്ഥി' എന്ന നിലയിലുള്ള നിങ്ങളുടെ  ഐഡന്റിറ്റി തെളിയിക്കേണ്ടതാണ്.</li>
-  <li>Prize winner should provide STUDENT PROOF OF IDENTIFICATION</li>
+  <li>വിജയികളാവുന്നവർ 'വിദ്യാർത്ഥി' എന്ന നിലയിലുള്ള നിങ്ങളുടെ  ഐഡന്റിറ്റി തെളിയിക്കേണ്ടതാണ്. (Prize winner should provide STUDENT PROOF OF IDENTIFICATION)</li>
 `,
   neutral_text: `Winners will be announced later`,
-  fail_text: 'Try later',
-  win_text: `You won!`,
-  sorry_text: `Sorry, there are no quizzes live right now. Check back later!`,
+  fail_text: '',
+  win_text: ``,
+  sorry_text: `Sorry, there are no contests live right now. Check back later!`,
 };
 
 export const api_url = 'https://wisdom-api.shahidh.in/v1/graphql';
